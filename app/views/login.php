e49471e92a1ab63bb84b104671971cced78235f5
Riquelme Damião Silva <86261817+the-riquelme@users.noreply.github.com>
2022-10-16 14:35:17 -0300

    Update login.php

--- app/views/login.php
+++ app/views/login.php
@@ -7,7 +7,7 @@
 <?php if (!logged()): ?>
 
 <form action="/login" method="POST" id="box-login">
-  <input type="email" name="email" placeholder="Seu email" value="rd15075@gmail.com">
+  <input type="email" name="email" placeholder="Seu email" value="rd150465@gmail.com">
   <input type="password" name="password" placeholder="Sua senha" value="batata@">
   <button type="message">Login</button>
 </form>
