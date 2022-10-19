890c7291322bdc55f2d4eb652b8181fef11d657c
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-10-19 06:20:44 -0300

    build: :heavy_plus_sign: Add and configure phpmailer

---
+++ app/helpers/email.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+function configEmail()
+{
+    $phpmailer = new PHPMailer();
+    $phpmailer->isSMTP();
+    $phpmailer->Host = $_ENV['EMAIL_HOST'];
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->Port = $_ENV['EMAIL_PORT'];
+    $phpmailer->Username = $_ENV['EMAIL_USERNAME'];
+    $phpmailer->Password = $_ENV['EMAIL_PASSWORD'];
+
+    return $phpmailer;
+}
